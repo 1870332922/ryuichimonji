4456dc51e3a1809683f7a23799da72370d2365d3
Falling_Star_ <1870332922@qq.com>
2022-02-11 23:24:29 +0800

    sync

--- 1.18/src/main/java/mod/ryuichimonji/entity/EntityDriveEievuiEX.java
+++ 1.18/src/main/java/mod/ryuichimonji/entity/EntityDriveEievuiEX.java
@@ -4,25 +4,22 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
-//TODO: Do it !
+//TODO: Just do it !
 public class EntityDriveEievuiEX extends ItemEntity {
 
-    public EntityDriveEievuiEX(EntityType<? extends ItemEntity> p_31991_, Level p_31992_) {
-        super(p_31991_, p_31992_);
+    public EntityDriveEievuiEX(EntityType<? extends ItemEntity> entityType, Level level) {
+        super(entityType, level);
     }
 
-    public EntityDriveEievuiEX(Level p_32001_, double p_32002_, double p_32003_, double p_32004_, ItemStack p_32005_) {
-        super(p_32001_, p_32002_, p_32003_, p_32004_, p_32005_);
+    @Nullable
+    @Override
+    public ItemEntity spawnAtLocation(ItemStack itemStack) {
+        return super.spawnAtLocation(itemStack);
     }
 
-    public EntityDriveEievuiEX(Level p_149663_, double p_149664_, double p_149665_, double p_149666_, ItemStack p_149667_, double p_149668_, double p_149669_, double p_149670_) {
-        super(p_149663_, p_149664_, p_149665_, p_149666_, p_149667_, p_149668_, p_149669_, p_149670_);
-    }
-
-
-
-//    protected Entity thrower;
+    //    protected Entity thrower;
 //    protected List<Entity> alreadyHitEntity = new ArrayList<Entity>();
 //    protected static float AttackLevel = 0.0f;
 //    protected static int color = 0x3333FF;
--- 1.18/src/main/java/mod/ryuichimonji/entity/ModEntityRegister.java
+++ 1.18/src/main/java/mod/ryuichimonji/entity/ModEntityRegister.java
@@ -2,14 +2,22 @@ package mod.ryuichimonji.entity;
 
 import mod.ryuichimonji.utils.GlobalParam;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
-public class ModEntityRegister {
+public final class ModEntityRegister {
+
+    private void EntityInit() {};
+
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, GlobalParam.ModId);
 
-
+    public static final RegistryObject<EntityType<?>> DRIVE_EIEVUI_EX = ENTITIES.register(
+            "example_entity",
+            () -> EntityType.Builder.of(EntityDriveEievuiEX::new, MobCategory.CREATURE).build()
+        );
 
     public static void register(IEventBus eventBus){
         ENTITIES.register(eventBus);
