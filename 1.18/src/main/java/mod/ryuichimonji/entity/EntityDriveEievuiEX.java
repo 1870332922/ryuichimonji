b3168dd84492c86b415de5ccfb0a9c609a1d3bfe
Falling_Star_ <1870332922@qq.com>
2022-02-11 18:32:07 +0800

    -

--- 1.18/src/main/java/mod/ryuichimonji/entity/EntityDriveEievuiEX.java
+++ 1.18/src/main/java/mod/ryuichimonji/entity/EntityDriveEievuiEX.java
@@ -1,7 +1,31 @@
 package mod.ryuichimonji.entity;
 
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
 //TODO: Do it !
-//public class EntityDriveEievuiEX extends Entity implements IThrowableEntity{
+public class EntityDriveEievuiEX extends Entity {
+
+    public EntityDriveEievuiEX(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Override
+    protected void defineSynchedData() {  }
+
+    @Override
+    protected void readAdditionalSaveData(CompoundTag p_20052_) {  }
+
+    @Override
+    protected void addAdditionalSaveData(CompoundTag p_20139_) {  }
+
+    @Override
+    public Packet<?> getAddEntityPacket() {
+        return null;
+    }
 
 //    protected Entity thrower;
 //    protected List<Entity> alreadyHitEntity = new ArrayList<Entity>();
@@ -327,4 +351,4 @@ package mod.ryuichimonji.entity;
 //
 //    }
 //
-//}
+}
